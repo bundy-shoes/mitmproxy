f014fd7c54905d6935f21c1b31bd4c81844c9644
Rotem Levi <rotem.levi@ironsrc.com>
2021-04-27 04:34:45 +0300

    .

--- mitmproxy/addons/upstream_auth.py
+++ mitmproxy/addons/upstream_auth.py
@@ -1,19 +1,16 @@
-import re
-import typing
-import base64
-
+from mitmproxy.http import HTTPFlow
 from mitmproxy import exceptions
 from mitmproxy import ctx
 from mitmproxy.utils import strutils
+from base64 import b64encode
 
 
-def parse_upstream_auth(auth):
-    pattern = re.compile(".+:")
-    if pattern.search(auth) is None:
+def parse_upstream_auth(username, password):
+    if username is None or password is None:
         raise exceptions.OptionsError(
-            "Invalid upstream auth specification: %s" % auth
-        )
-    return b"Basic" + b" " + base64.b64encode(strutils.always_bytes(auth))
+            "Invalid upstream auth specification: %s %s" % username, password)
+    return b"Basic" + b" " + b64encode(
+        strutils.always_bytes(f"{username}:{password}"))
 
 
 class UpstreamAuth():
@@ -27,38 +24,110 @@ class UpstreamAuth():
         - Reverse proxy regular requests (CONNECT is invalid in this mode)
     """
     def __init__(self):
-        self.auth = None
 
-    def load(self, loader):
-        loader.add_option(
-            "upstream_auth", typing.Optional[str], None,
-            """
-            Add HTTP Basic authentication to upstream proxy and reverse proxy
-            requests. Format: username:password.
-            """
+        ctx.log.info(
+            "\n--------------------------------sessions init--------------------------------\n"
         )
 
-    def configure(self, updated):
-        # FIXME: We're doing this because our proxy core is terminally confused
-        # at the moment. Ideally, we should be able to check if we're in
-        # reverse proxy mode at the HTTP layer, so that scripts can put the
-        # proxy in reverse proxy mode for specific requests.
-        if "upstream_auth" in updated:
-            if ctx.options.upstream_auth is None:
-                self.auth = None
-            else:
-                if ctx.options.upstream_auth:  # pragma: no cover
-                    ctx.log.warn("upstream_auth is currently nonfunctioning, "
-                                 "see https://github.com/mitmproxy/mitmproxy/issues/4348")
-                self.auth = parse_upstream_auth(ctx.options.upstream_auth)
+        self.fall_back = ""
+        self.zone = ""
+        self.password = ""
+        self.user_id = ""
+        self.country = ""
+        self.state = ""
+        self.asn = ""
+        self.city = ""
+        self.session = ""
 
-    def http_connect(self, f):
-        if self.auth and f.mode == "upstream":
+    def load(self, loader):
+        loader.add_option(name="upstream_lum_auth_zone",
+                          typespec=str,
+                          default='',
+                          help="""""")
+
+        loader.add_option(name="upstream_lum_auth_user_id",
+                          typespec=str,
+                          default='',
+                          help="""""")
+
+        loader.add_option(name="upstream_lum_auth_password",
+                          typespec=str,
+                          default='',
+                          help="""""")
+
+        loader.add_option(name="upstream_lum_auth_fall_back",
+                          typespec=str,
+                          default='',
+                          help="""""")
+
+    def configure(self, updated):
+        ctx.log.info(
+            "\n--------------------------------configure addon--------------------------------\n"
+        )
+
+        if "upstream_lum_auth_fall_back" in updated and ctx.options.upstream_lum_auth_fall_back:
+            self.fall_back = ctx.options.upstream_lum_auth_fall_back
+
+        if "upstream_lum_auth_zone" in updated and ctx.options.upstream_lum_auth_zone:
+            self.zone = ctx.options.upstream_lum_auth_zone
+
+        if "upstream_lum_auth_password" in updated and ctx.options.upstream_lum_auth_password:
+            self.password = ctx.options.upstream_lum_auth_password
+
+        if "upstream_lum_auth_user_id" in updated and ctx.options.upstream_lum_auth_user_id:
+            self.user_id = ctx.options.upstream_lum_auth_user_id
+
+        if "upstream_lum_country" in updated and ctx.options.upstream_lum_country:
+            self.country = f"-country-{str.lower(ctx.options.upstream_lum_country)}"
+
+        if "upstream_lum_state" in updated and ctx.options.upstream_lum_state:
+            self.state = f"-state-{str.lower(ctx.options.upstream_lum_state)}"
+
+        if "upstream_lum_asn" in updated and ctx.options.upstream_lum_asn:
+            self.asn = f"-state-{ctx.options.upstream_lum_asn}"
+
+        if "upstream_lum_city" in updated and ctx.options.upstream_lum_city:
+            self.city = f"-city-{ctx.options.upstream_lum_city}"
+
+        if "upstream_lum_session" in updated and ctx.options.upstream_lum_session:
+            self.session = ctx.options.upstream_lum_session
+
+    @property
+    def auth(self):
+        ctx.log.info(
+            "\n--------------------------------authenticate proxy request--------------------------------\n"
+        )
+        username = f"lum-customer-{self.user_id}-zone-{self.zone}{self.fall_back}{self.country}{self.state}{self.city}{self.asn}-session-{self.session}"
+        return parse_upstream_auth(username=username, password=self.password)
+
+    def http_connect(self, f: HTTPFlow):
+        ctx.log.info(
+            "\n--------------------------------http_connect--------------------------------\n"
+        )
+        if f.mode == "upstream":
+            f.request.headers["Proxy-Authorization"] = self.get_auth(f)
+
+    def requestheaders(self, f: HTTPFlow):
+        ctx.log.info(
+            "\n--------------------------------requestheaders--------------------------------\n"
+        )
+
+        if f.mode == "upstream" and not f.server_conn.via:
+            f.request.headers["Proxy-Authorization"] = self.auth
+        elif ctx.options.mode.startswith("reverse"):
             f.request.headers["Proxy-Authorization"] = self.auth
 
-    def requestheaders(self, f):
-        if self.auth:
-            if f.mode == "upstream" and not f.server_conn.via:
-                f.request.headers["Proxy-Authorization"] = self.auth
-            elif ctx.options.mode.startswith("reverse"):
-                f.request.headers["Proxy-Authorization"] = self.auth
+    def response(self, f: HTTPFlow):
+        """
+            The full HTTP response has been read.
+        """
+        status_code = f.response.status_code
+        ctx.log.info(f"status_code: {status_code}")
+
+    def error(self, flow: HTTPFlow):
+        """
+            An HTTP error has occurred, e.g. invalid server responses, or
+            interrupted connections. This is distinct from a valid server HTTP
+            error response, which is simply a response with an HTTP error code.
+        """
+        ctx.log.error(flow.error)
