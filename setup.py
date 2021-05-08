6e5f4cb22510d6c5ba5245d8efcb2944b62af9a2
Rotem Levi <rotem.levi@ironsrc.com>
2021-05-08 23:10:48 +0300

    added beautifulsoup4 to requirements added env HOME for mitmproxy . folder.

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements/#install-requires
     # It is not considered best practice to use install_requires to pin dependencies to specific versions.
     install_requires=[
+        "beautifulsoup4==4.9.3",
         "asgiref>=3.2.10,<3.4",
         "blinker>=1.4, <1.5",
         "Brotli>=1.0,<1.1",
